2aae0616b6509f1db946f631c76d268d5d74a52b
inpinseptipin <32192460+inpinseptipin@users.noreply.github.com>
2022-02-02 00:41:44 -0500

    AudioEffect v1 Commit

---
+++ AudioEffect.h
@@ -0,0 +1,97 @@
+#ifndef AuxPort_AudioEffect_H
+#define AuxPort_AudioEffect_H
+#pragma once
+/*
+*			AuxPort Wrapper over Rackfx
+			"I tried to make it easier" - inpinseptipin
+
+			BSD 3-Clause License
+
+			Copyright (c) 2022, Satyarth Arora, Teaching Assistant, University of Miami
+			All rights reserved.
+
+			Redistribution and use in source and binary forms, with or without
+			modification, are permitted provided that the following conditions are met:
+
+			1. Redistributions of source code must retain the above copyright notice, this
+			   list of conditions and the following disclaimer.
+
+			2. Redistributions in binary form must reproduce the above copyright notice,
+			   this list of conditions and the following disclaimer in the documentation
+			   and/or other materials provided with the distribution.
+
+			3. Neither the name of the copyright holder nor the names of its
+			   contributors may be used to endorse or promote products derived from
+			   this software without specific prior written permission.
+
+			THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+			AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+			IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+			DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+			FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+			DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+			SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+			CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+			OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+			OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+#include <vector>
+#include <math.h>
+#include "plugincore.h"
+namespace AuxPort
+{
+	template<class bufferType, class effectType>
+	class Effect
+	{
+	public:
+/*===================================================================================*/
+/*
+	[Constructor] Safely Deallocates the memory
+*/
+		Effect<bufferType, effectType>() = default;
+/*===================================================================================*/
+/*
+	[Destructor] Safely Deallocates the memory
+*/
+		~Effect<bufferType, effectType>() = default;
+/*===================================================================================*/
+/*
+	[Copy Constructor] Safely Copies memory from one Volume Object to another
+*/
+		Effect<bufferType, effectType>(const Effect<bufferType, effectType>& kernel) = default;
+/*===================================================================================*/
+/*
+	[Function] Set your Control Addresses (DONT MESS WITH IT)
+*/
+		void push(effectType* parameterAddress)
+		{
+			_controls.push_back(parameterAddress);
+		}
+/*===================================================================================*/
+/*
+	[Function] Implement your Frame DSP Logic here
+*/
+		bufferType run(const bufferType& frameValue) 
+		{
+			/*===================================================================================*/
+			/*
+				Write DSP Algorithm here
+			*/
+			/*===================================================================================*/
+			return frameValue;
+		}
+	private:
+		std::vector<effectType*> _controls;
+/*===================================================================================*/
+/*
+	[Function] Gets the Control from our nice dandy vector of pointers (DONT MESS WITH IT)
+*/
+		effectType getControl(const int& i)
+		{
+			return *_controls[i];
+		}
+	};
+
+}
+#endif
